3192578f837ca1a6ef5d1dd73e96a2422fa232aa
Anushka Shrivastava <57841081+anushka00@users.noreply.github.com>
2022-06-18 23:56:16 +0530

    Update longestsubarrayzero.java

--- day 4/longestsubarrayzero.java
+++ day 4/longestsubarrayzero.java
@@ -1,60 +1,33 @@
-class Solution {
-    public int longestConsecutive(int[] arr) {
-        if(arr.length==0)
-        {
-            return 0;
-        }
-        
-//         Arrays.sort(nums);
-        
-//         int prev=nums[0];
-//         int longsub=1;
-//         int ans=1;
-//         // as each element in itself is a sub sequence both for longsub and ans.
-        
-//         for(int i=1;i<nums.length;i++){
-            
-//             if(nums[i] == prev+1)
-//             {
-//                 longsub+=1;
-                
-//             }
-//             else if(nums[i]!= prev)
-//             {
-//                 longsub=1;
-//             }
-//             prev= nums[i];
-//             ans= Math.max(ans, longsub);
-            
-//         } nlogn approach
-        // o(n) approach 
-        HashSet<Integer> set= new HashSet<>();
-           for(int num:arr)
-        {
-            set.add(num);
-        }
-        int longcount=0;
-        
-        for(int num:arr)
-        {
-            if( !set.contains(num-1))
-            {
-             int currentnum=num;
-                int currentcount=1;
-                
-                while(set.contains(currentnum+1))
-                {
-                    currentcount+=1;
-                    currentnum+=1;
-                }
-                longcount= Math.max(longcount, currentcount);
+lass solution
+{
+    int maxLen(int A[], int n)
+    {
+        // Your code here
+          HashMap<Integer, Integer> mpp = new HashMap<Integer, Integer>();
+
+        int maxi = 0;
+        int sum = 0; 
+
+        for(int i = 0;i<n;i++) {
+
+            sum += A[i]; 
+
+            if(sum == 0) {
+                maxi = i + 1; 
             }
-            
+            else {
+                if(mpp.get(sum) != null) {
+
+                    maxi = Math.max(maxi, i - mpp.get(sum)); 
+                }
+                else {
+
+                    mpp.put(sum, i); 
+                }
+            }
         }
+        return maxi; 
         
-    return longcount; 
         
-        
-     
     }
 }
